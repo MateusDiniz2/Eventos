133af70ddb192e9c83a7e1f0c98fb4b3fb5d355a
Mateus Gomes <mateus.gomes@astrein.com>
2022-05-18 15:18:35 -0300

    Retirando using

--- Eventos.API/Controllers/EventosController.cs
+++ Eventos.API/Controllers/EventosController.cs
@@ -3,8 +3,6 @@ using Eventos.Domain;
 using Eventos.Persistence.Context;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Eventos.API.Controllers
